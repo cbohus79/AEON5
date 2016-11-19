bb3640921da556872e3432e94e79af886b6aaae5
rickdodgeman <rdodgeman4@mailinator.com>
2016-11-19 13:23:03 +0000

    Update aeon-multisensor-4-gen5-configurable.groovy

--- aeon-multisensor-4-gen5-configurable.groovy
+++ aeon-multisensor-4-gen5-configurable.groovy
@@ -10,7 +10,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Modified by john.fullman@gmail.com
+ *  Modified by john.fullman@gmail.com and rickdodgerman
  */
 metadata
 {
